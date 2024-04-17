dd91ba41523a40b1d0fd14403c270209f7a9cff4
Arthur Garcia Torrezani <torrezaniarthur@gmail.com>
2024-04-17 13:55:13 -0300

    Server iniciado, com express, sucrase e nodemon

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express, { json } from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+    this.routes();
+  }
+
+  middlewares() {
+    // register all application middlewares
+    this.server.use(json()); // send request and receive answers with json
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
